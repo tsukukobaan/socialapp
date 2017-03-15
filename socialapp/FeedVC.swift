e0963076c09df39f2059555c97cda983a8a369be
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-15 18:38:52 +0900

    extracting data for rows not shown in UI yet

--- socialapp/FeedVC.swift
+++ socialapp/FeedVC.swift
@@ -13,6 +13,8 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observe(.value, with: {(snapshot) in
             
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String,Any>{
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
+            
+//            if let content = snapshot.value {
+//                print(content)
+//            }
+            
         
         
         })
@@ -35,10 +53,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
---
+++ socialapp/Post.swift
@@ -0,0 +1,60 @@
+//
+//  Post.swift
+//  socialapp
+//
+//  Created by 小林 泰 on 2017/03/15.
+//  Copyright © 2017年 TokyoIceHockeyChannel. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: String {
+        return _postKey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._imageUrl = imageUrl
+        self._likes = likes
+    }
+    
+    init(postKey: String, postData: Dictionary<String,Any>) {
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String{
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String{
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
