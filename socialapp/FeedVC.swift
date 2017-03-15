bab985621ba61152cc38a38187dab2fdbf8dfbe2
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-15 20:36:52 +0900

    download pics from firebase and cache em

--- socialapp/FeedVC.swift
+++ socialapp/FeedVC.swift
@@ -17,6 +17,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
+    static var imageCache: NSCache<NSString, UIImage> = NSCache()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,8 +85,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
             
-            cell.configureCell(post: post)
-            return cell
+//            var img: UIImage!
+            
+            if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
+                cell.configureCell(post: post,img: img)
+                return cell
+            } else {
+                cell.configureCell(post: post, img: nil)
+                return cell
+            }
             
         } else {
             return PostCell()
--- socialapp/PostCell.swift
+++ socialapp/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -23,10 +24,35 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage?) {
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            
+                let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+                ref.data(withMaxSize: 2 * 1024 * 1024,completion: {(data,error) in
+                    
+                    if error != nil {
+                        print("JESS: unable to download from Firebase storage")
+                    } else {
+                        print("JESS: Image downloaded from image ")
+                        if let imageData = data {
+                            if let img = UIImage(data: imageData) {
+                                self.postImg.image = img
+                                FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                            }
+                        }
+                    }
+                
+                })
+            
+        }
+        
+        
     }
 
     
